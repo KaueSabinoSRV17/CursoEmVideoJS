a2cf221018b76e13f901b9d7d71db17cb66cfd30
Kauê Sabino VIlaça de Almeida <kauevillaca2018@gmail.com>
2022-04-23 10:18:57 -0300

    build: Criação de modelos HTML e CSS

---
+++ Módulos/B/Aula 4/hello.js
@@ -0,0 +1,21 @@
+// fazendo do jeito clássico
+alert('Olá Mundo - aperte esc se quiser cancelar o código');
+
+confirm('Pode me informar o seu nome?');
+
+var nome = prompt('Obrigado, pode escreve-lo por favor!');
+
+alert(nome);
+
+
+// tentando fazer um pouco melhor
+
+// function botao() {
+    
+//     campo = document.getElementById('nome');
+
+//     nome = campo.value
+
+//     document.write(nome)
+
+// }
---
+++ Módulos/B/Aula 5/vars.js
@@ -0,0 +1,4 @@
+// usando uma lógica um pouco mais avançada, armazenando o valor que um usuário vai inserir no prompt
+var nome = prompt('Me diga por favor o seu nome');
+
+alert(nome);

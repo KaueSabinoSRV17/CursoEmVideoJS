db09b6310417570497343e8e11efd16cbf97d361
Kauê Sabino VIlaça de Almeida <kauevillaca2018@gmail.com>
2022-04-28 20:35:03 -0300

    feat: utilizado o DOM para controlar alerts

--- Módulos/B/Aula 4/hello.js
+++ Módulos/B/Aula 4/hello.js
@@ -1,21 +1,33 @@
-// fazendo do jeito clássico
-alert('Olá Mundo - aperte esc se quiser cancelar o código');
-
-confirm('Pode me informar o seu nome?');
-
-var nome = prompt('Obrigado, pode escreve-lo por favor!');
-
-alert(nome);
-
-
-// tentando fazer um pouco melhor
-
-// function botao() {
+// Códigos ensinados permanecerão, mas serão refatorados com o DOM, para que sejam disparados apenas por demanda do usuário
+function botao() {
     
-//     campo = document.getElementById('nome');
+    alert('Olá Mundo - aperte esc se quiser cancelar o código');
 
-//     nome = campo.value
+    var escolha = confirm('Pode me informar o seu nome?');
 
-//     document.write(nome)
+    if (escolha == true) {
 
-// }
+        var nome = prompt('Obrigado, pode escreve-lo por favor!');
+    
+        alert(nome);
+        
+    } else {
+
+        alert('Ok. tenha um bom dia!')
+
+    }    
+
+}
+
+
+/* tentando fazer um pouco melhor (percebe minha falha? SIm, não declarei que nada era uma variável kkkkk. como pude)
+
+ function botao() {
+    
+     campo = document.getElementById('nome');
+
+     nome = campo.value
+
+     document.write(nome)
+
+ } */

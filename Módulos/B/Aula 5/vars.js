d5aee3fbbd342b4375c245d6f4344008b12e3c99
Kauê Sabino VIlaça de Almeida <kauevillaca2018@gmail.com>
2022-04-28 21:27:13 -0300

    feat: código ativado através do DOM

--- Módulos/B/Aula 5/vars.js
+++ Módulos/B/Aula 5/vars.js
@@ -1,4 +1,8 @@
 // usando uma lógica um pouco mais avançada, armazenando o valor que um usuário vai inserir no prompt
-var nome = prompt('Me diga por favor o seu nome');
+function botao() {
 
-alert(nome);
+    var nome = prompt('Me diga por favor o seu nome');
+
+    alert(nome); //guardamos o valor de qualquer coisa que for digitada no prompt atribuindo ele em si como o valor de uma variável
+    
+}

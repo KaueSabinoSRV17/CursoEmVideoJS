1657d1bfc1ab706348a7059785d30f4457a123f9
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-07-25 19:12:34 -0300

    Aula 8 - Operadores Relacionais e Lógicos

---
+++ assets/scripts/modulos/b/aula-8-operadores-b.js
@@ -0,0 +1,3 @@
+alert(`Operadores Relacionais\n>: maior\n<: menor\n>=: maior ou igual\n<= menor ou igual\n==: igual\n===: igual com o mesmo tipo\n!=: diferente\n\nEstes operadores sempre retornam booleanos`)
+alert(`Operadores Lógicos\n!: Negação\n&& Conjunção (E)\n|| Disjunção (Ou)`)
+alert(`Ordem de Precedência Suprema\n1- Aritméticos (Contas)\n2- Relacionais\n3- Lógicos (! -> && -> ||)`)
